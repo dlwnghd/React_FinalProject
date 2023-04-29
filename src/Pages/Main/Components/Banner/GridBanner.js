4ab9bb7780c20eda9f0256cca06a9c1883074e4a
beenbin <77373566+showme0241@users.noreply.github.com>
2023-04-29 23:20:07 +0900

    :sparkles: Feat : 메인 페이지 > 최근 등록 상품 섹션 슬라이더 기능 추가

--- src/Pages/Main/Components/Banner/GridBanner.js
+++ src/Pages/Main/Components/Banner/GridBanner.js
@@ -1,32 +1,89 @@
 import styled from 'styled-components'
-import { ColumnNumberCSS, GridCenterCSS } from '../../../../Styles/common'
+import {
+	ColumnNumberCSS,
+	FlexCenterCSS,
+	GridCenterCSS,
+} from '../../../../Styles/common'
+import productsMock from '../../../../__mock__/Data/Product/product.data'
+import { useEffect, useRef, useState } from 'react'
+import { Arrow_Icon } from '../../../../Components/Icons/Icons'
 
 function GridBanner() {
+	const [currentX, setCurrentX] = useState(0)
+	const slider = useRef(null)
+
+	const nextSlide = () => {
+		if (currentX < productsMock.slice(0, 4).length - 1) {
+			setCurrentX(currentX + 1)
+			console.log(currentX)
+		}
+	}
+
+	const prevSlide = () => {
+		if (currentX > 0) {
+			setCurrentX(currentX - 1)
+		}
+	}
+
+	useEffect(() => {
+		slider.current.style.transform = `translateX(-${currentX}00%)`
+	}, [currentX])
+
+	const [startX, setStartX] = useState(0)
+	const [endX, setEndX] = useState(0)
+
+	const onTouchStart = e => {
+		setStartX(e.touches[0].clientX)
+	}
+	const onTouchMove = e => {
+		setEndX(e.touches[0].clientX)
+	}
+	const onTouchEnd = () => {
+		if (endX > startX) {
+			prevSlide()
+		}
+
+		if (startX > endX) {
+			nextSlide()
+		}
+	}
+
 	return (
 		<S.Wrapper>
 			<S.Title>최근 등록 상품</S.Title>
 			<S.SlideContainer>
-				<S.SlideList>
-					<S.SlideBox>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-					</S.SlideBox>
-					<S.SlideBox>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-					</S.SlideBox>
+				<S.SlideList
+					ref={slider}
+					onTouchStart={onTouchStart}
+					onTouchMove={onTouchMove}
+					onTouchEnd={onTouchEnd}
+					// onMouseDown={onMouseDown}
+					// onMouseMove={onMouseMove}
+					// onMouseLeave={onMouseLeave}
+				>
+					{productsMock.slice(0, 4).map((item, idx) => {
+						return (
+							<S.SlideBox key={idx}>
+								{/* createdAt을 기준으로 sort */}
+								{productsMock.slice(0, 6).map((item, idx) => {
+									return (
+										<S.SlideItem
+											key={idx}
+											recentIMG={`${item.image_url}`}
+										></S.SlideItem>
+									)
+								})}
+							</S.SlideBox>
+						)
+					})}
 				</S.SlideList>
 				<S.ButtonBox>
-					<button className="prev">&lt;</button>
-					<button className="next">&gt;</button>
+					<button className="prev" onClick={prevSlide}>
+						<Arrow_Icon size="15" />
+					</button>
+					<button className="next" onClick={nextSlide}>
+						<Arrow_Icon size="15" />
+					</button>
 				</S.ButtonBox>
 			</S.SlideContainer>
 		</S.Wrapper>
@@ -39,7 +96,7 @@ const Wrapper = styled.section`
 	overflow: hidden;
 `
 
-const Title = styled.h4`
+const Title = styled.h3`
 	text-align: center;
 `
 
@@ -52,21 +109,28 @@ const SlideList = styled.div`
 	${GridCenterCSS}
 	grid-auto-flow: column;
 	grid-auto-columns: 100%;
+	transition: 0.5s ease-in-out;
 `
 
 const SlideBox = styled.ul`
 	${GridCenterCSS}
 	${ColumnNumberCSS(6)}
     box-sizing: border-box;
+
 	& > li {
 		width: 17.4rem;
 		height: 17.4rem;
-		background: ${({ theme }) => theme.COLOR.common.gray[100]};
 	}
 
 	@media screen and (max-width: ${({ theme }) => theme.MEDIA.mobile}) {
 		${ColumnNumberCSS(3)}
 	}
+`
+
+const SlideItem = styled.li`
+	cursor: pointer;
+	background: ${({ recentIMG }) => `url(${recentIMG})`} no-repeat center center;
+	background-size: cover;
 `
 
 const ButtonBox = styled.div`
@@ -79,13 +143,18 @@ const ButtonBox = styled.div`
 		height: 6rem;
 		border: none;
 		box-sizing: border-box;
-		background: ${({ theme }) => theme.COLOR.common.gray[300]};
+		background: ${({ theme }) => theme.COLOR.common.white};
+		${FlexCenterCSS}
 	}
 
 	& > .prev {
 		left: 0;
 		border-top-right-radius: 1rem;
 		border-bottom-right-radius: 1rem;
+
+		& > svg {
+			transform: rotate(180deg);
+		}
 	}
 
 	& > .next {
@@ -101,5 +170,6 @@ const S = {
 	SlideContainer,
 	SlideList,
 	SlideBox,
+	SlideItem,
 	ButtonBox,
 }
