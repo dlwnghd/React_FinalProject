dedfe1ef0bef92e5b680783dcd51013584c1f408
beenbin <77373566+showme0241@users.noreply.github.com>
2023-04-29 23:54:10 +0900

    :lipstick: Design : 전역 글로벌 스타일링 파일 수정

--- src/Styles/global.js
+++ src/Styles/global.js
@@ -38,18 +38,22 @@ const GlobalStyles = createGlobalStyle`
 
     h1 {
         // 로고
+        font-size:${({ theme }) => theme.FONT_SIZE.big};
     }
 
     h2 {
-        font-size:${({ theme }) => theme.FONT_SIZE.huge}
+        font-size:${({ theme }) => theme.FONT_SIZE.huge};
+        font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
     }
 
     h2 ~ p {
         font-size:${({ theme }) => theme.FONT_SIZE.large}
+        
     }
 
     h3 {
-        font-size:${({ theme }) => theme.FONT_SIZE.large}
+        font-size:${({ theme }) => theme.FONT_SIZE.large};
+        font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
     }
 
     h3 ~ p {
@@ -57,7 +61,8 @@ const GlobalStyles = createGlobalStyle`
     }
 
     h4 {
-        font-size:${({ theme }) => theme.FONT_SIZE.medium}
+        font-size:${({ theme }) => theme.FONT_SIZE.medium};
+        font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
     }
 
     textarea {
@@ -65,9 +70,8 @@ const GlobalStyles = createGlobalStyle`
     }
 
     p {
-        font-weight: 700;
         font-size:${({ theme }) => theme.FONT_SIZE.small};
-        font-weight:${({ theme }) => theme.FONT_WEIGHT.bold}
+        font-weight:${({ theme }) => theme.FONT_WEIGHT.regular}
     }
 
     span {
