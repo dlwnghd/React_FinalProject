114a4918bb276e5c887cd10901e31f6f5cc38e3f
beenbin <77373566+showme0241@users.noreply.github.com>
2023-04-30 19:29:32 +0900

    :sparkles: Feat : 공용 컴포넌트 Filter 작업 완료

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,33 +1,113 @@
 import styled from 'styled-components'
+import { DropdownArrow_Icon } from '../Icons/Icons'
+import { useRef } from 'react'
+import { useState } from 'react'
 
-function Filter(/* filterArray */) {
-	// filter Array는 받아올 때부터 option에 사용하는 모든 것을 배열로 전달받는다.
-	const filterArray = ['a', 'b']
+function Filter({ filter }) {
+	// prop으로 전달받은 배열 데이터 : Search > filter
+	// 배열에 인수가 3개이면 border X
+
+	const newFilter = filter.sort((a, b) => {
+		return a.length - b.length
+	})
+
+	const selectRef = useRef(null)
+	const [isSelect, setIsSelect] = useState(false)
+	const [changeText, setChangeText] = useState(newFilter[0])
+
+	const onSelect = () => {
+		setIsSelect(prev => !prev)
+	}
+
+	const onChange = e => {
+		setChangeText(e.target.innerText)
+		setIsSelect(prev => !prev)
+	}
+
+	const onLeave = () => {
+		setIsSelect(prev => !prev)
+	}
 
 	return (
-		<S.SelectContainer>
-			{filterArray.map(title => {
-				return <SelectList>{title}</SelectList>
-			})}
-		</S.SelectContainer>
+		<Wrapper>
+			<S.SelectContainer onMouseLeave={onLeave}>
+				<S.DefaultBox number={newFilter.length} onClick={onSelect}>
+					{changeText}
+					<DropdownArrow_Icon size="16" />
+				</S.DefaultBox>
+				{isSelect && (
+					<S.SelectList
+						ref={selectRef}
+						number={newFilter.length}
+						onClick={onChange}
+					>
+						{filter.map((title, idx) => {
+							return <S.SelectBox key={idx}>{title}</S.SelectBox>
+						})}
+					</S.SelectList>
+				)}
+			</S.SelectContainer>
+		</Wrapper>
 	)
 }
 
 export default Filter
 
-const SelectContainer = styled.select`
-	// 필터에 담겨진 글자의 길이가 다르기 때문에 제일 길이가 긴 글자를 기준으로
-	// 셀렉트 박스의 width값을 설정 (padding : maxLengthText의 글자크기 * 글자수 / 2 )
-	padding: 0 ${({ title }) => title};
-	height: 6rem;
-	border-radius: 3rem;
+const Wrapper = styled.div`
+	width: 14rem;
+	// 부모 컴포넌트 relative
+	position: absolute;
+	top: 0;
+	right: 0;
+`
+const SelectContainer = styled.div`
+	width: 100%;
 `
 
-const SelectList = styled.option`
+const DefaultBox = styled.button`
+	position: relative;
 	font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+	padding: 1rem 0;
+	border-radius: 1rem;
+	border: ${({ number }) => (number <= 3 ? 0 : 0.1)}rem solid
+		${({ theme }) => theme.COLOR.common.gray[300]};
+	width: 100%;
+	background: none;
+
+	& > svg {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		right: 1.2rem;
+	}
+`
+
+const SelectList = styled.ul`
+	text-align: center;
+	border-radius: ${({ number }) => (number <= 3 ? 0 : 1)}rem;
+	overflow: hidden;
+	font-size: ${({ theme }) => theme.FONT_SIZE.tiny};
+	border: 0.1rem solid ${({ theme }) => theme.COLOR.common.gray[300]};
+`
+
+const SelectBox = styled.li`
+	padding: 1rem 0;
+	border-bottom: 0.1rem solid ${({ theme }) => theme.COLOR.common.gray[300]};
+
+	&:last-of-type {
+		border-bottom: none;
+	}
+
+	&:hover {
+		font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
+		background: ${({ theme }) => theme.COLOR.hover};
+	}
 `
 
 const S = {
+	Wrapper,
+	DefaultBox,
 	SelectContainer,
 	SelectList,
+	SelectBox,
 }
