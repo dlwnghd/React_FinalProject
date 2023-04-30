b6be8510d9cfbf382e5b76c574ba0008e331605f
beenbin <77373566+showme0241@users.noreply.github.com>
2023-04-30 19:36:24 +0900

    :lipstick: Design : 메인 페이지 UI 스몰 배너 작업중

--- src/Pages/Main/Components/Banner/SlideBanner.js
+++ src/Pages/Main/Components/Banner/SlideBanner.js
@@ -1,26 +1,28 @@
 import styled from 'styled-components'
-import {
-	ColumnNumberCSS,
-	FlexAlignCSS,
-	GridCenterCSS,
-} from '../../../../Styles/common'
+import { ColumnNumberCSS, GridCenterCSS } from '../../../../Styles/common'
+import productsMock from '../../../../__mock__/Data/Product/product.data'
 
 function SlideBanner() {
+	console.log(productsMock)
 	return (
 		<S.Wrapper>
 			<S.Container>
-				<S.FirstSlideList>
-					<S.ItemBox></S.ItemBox>
-					<S.ItemBox></S.ItemBox>
-					<S.ItemBox></S.ItemBox>
-					<S.ItemBox></S.ItemBox>
-				</S.FirstSlideList>
-				<S.SecondSlideList>
-					<S.ItemBox></S.ItemBox>
-					<S.ItemBox></S.ItemBox>
-					<S.ItemBox></S.ItemBox>
-					<S.ItemBox></S.ItemBox>
-				</S.SecondSlideList>
+				<S.SlideList>
+					<S.FirstSlideBox>
+						<S.SlideItems></S.SlideItems>
+					</S.FirstSlideBox>
+					<S.PaginationBox>
+						<S.PaginationItems></S.PaginationItems>
+					</S.PaginationBox>
+				</S.SlideList>
+				<S.SlideList>
+					<S.SecondSlideBox>
+						<S.SlideItems></S.SlideItems>
+					</S.SecondSlideBox>
+					<S.PaginationBox>
+						<S.PaginationItems></S.PaginationItems>
+					</S.PaginationBox>
+				</S.SlideList>
 			</S.Container>
 		</S.Wrapper>
 	)
@@ -37,35 +39,51 @@ const Container = styled.div`
 	${ColumnNumberCSS(2)}
     column-gap: 0;
 	overflow: hidden;
-
-	& > ul {
-		width: 100%;
-		${FlexAlignCSS}
-	}
 `
 
-const FirstSlideList = styled.ul`
+const SlideList = styled.div`
+	width: 100%;
+	position: relative;
+`
+
+const FirstSlideBox = styled.ul`
 	& > li {
 		background: ${({ theme }) => theme.COLOR.common.gray[300]};
 	}
 `
 
-const SecondSlideList = styled.ul`
+const SecondSlideBox = styled.ul`
 	& > li {
 		background: ${({ theme }) => theme.COLOR.common.gray[200]};
 	}
 `
 
-const ItemBox = styled.li`
+const SlideItems = styled.li`
 	flex-shrink: 0;
 	width: 100%;
 	height: 17.4rem;
 `
 
+const PaginationBox = styled.div``
+
+const PaginationItems = styled.div`
+	position: absolute;
+	bottom: 1rem;
+	left: 50%;
+	transform: translateX(-50%);
+	width: 1rem;
+	height: 1rem;
+	border-radius: 50%;
+	background: ${({ theme }) => theme.COLOR.common.gray[100]};
+`
+
 const S = {
 	Wrapper,
 	Container,
-	FirstSlideList,
-	SecondSlideList,
-	ItemBox,
+	SlideList,
+	FirstSlideBox,
+	SecondSlideBox,
+	SlideItems,
+	PaginationBox,
+	PaginationItems,
 }
