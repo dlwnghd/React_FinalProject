52da242239c21915d12b634231581049b9320697
taeginote <ab315601@naver.com>
2023-04-30 21:30:53 +0900

    💄Design : 물품등록 퍼블리싱 완료

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 import Images from './Components/Images'
-import { WidthAutoCSS } from '../../Styles/common'
+import { FlexCenterCSS, WidthAutoCSS } from '../../Styles/common'
 import ItemInfo from './Components/ItemInfo'
 import Map from './Components/Map'
+import Button from '../../Components/Button/Button'
 
 function Register() {
 	return (
@@ -10,6 +11,10 @@ function Register() {
 			<Images />
 			<ItemInfo />
 			<Map />
+			<S.ButtonWrap>
+				<Button style={{ margin: '4rem' }}>등록 완료</Button>
+				<Button style={{ margin: '4rem' }}>취소</Button>
+			</S.ButtonWrap>
 		</S.Wrapper>
 	)
 }
@@ -19,4 +24,8 @@ export default Register
 const Wrapper = styled.div`
 	${WidthAutoCSS}
 `
-const S = { Wrapper }
+const ButtonWrap = styled.div`
+	${FlexCenterCSS}
+`
+
+const S = { Wrapper, ButtonWrap }
