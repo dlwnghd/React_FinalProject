519f77f2cc4ffcd9984fde0f3391501fd33b70a2
beenbin <77373566+showme0241@users.noreply.github.com>
2023-04-30 21:52:59 +0900

    :art: Feat : 찜(하트) 안 눌러지는 코드 구조 개선

--- src/Components/ItemBox/ItemBox.js
+++ src/Components/ItemBox/ItemBox.js
@@ -19,14 +19,13 @@ function ItemBox({ posterPath, title, context, price, isLiked, ...rest }) {
 	const onEdit = () => {}
 
 	return (
-		<S.Wrapper {...rest}>
-			<S.IMGContainer posterIMG={posterPath}>
-				{!isHeart ? (
-					<NotFillHeart_Icon size="20" onClick={onHeart} />
-				) : (
-					<FillHeart_Icon size="20" onClick={onHeart} />
-				)}
-			</S.IMGContainer>
+		<S.Wrapper>
+			{!isHeart ? (
+				<NotFillHeart_Icon size="30" onClick={onHeart} />
+			) : (
+				<FillHeart_Icon size="30" onClick={onHeart} />
+			)}
+			<S.IMGContainer posterIMG={posterPath} {...rest}></S.IMGContainer>
 			<S.DescContainer>
 				<S.DescBox context={context}>
 					<h4>{title}</h4>
@@ -47,37 +46,30 @@ export default ItemBox
 const Wrapper = styled.div`
 	${FlexBetweenCSS}
 	flex-direction: column;
-	cursor: pointer;
+	position: relative;
 	width: 100%;
 	height: 100%;
 	box-sizing: border-box;
 	overflow: hidden;
+
+	& > svg {
+		position: absolute;
+		z-index: 999;
+		cursor: pointer;
+		top: 1.4rem;
+		right: 1.4rem;
+		color: red;
+		// 파람으로 보낼 데이터의 디폴트와 변수를 구분해서 삼항 연산자로 정리
+	}
 `
 
 const IMGContainer = styled.div`
 	position: relative;
+	cursor: pointer;
 	width: 100%;
 	height: 27.6rem;
 	background: ${({ posterIMG }) => `url(${posterIMG})`} no-repeat center center;
 	background-size: cover;
-
-	& > button {
-		position: absolute;
-		top: 2rem;
-		right: 2rem;
-		background: none;
-		border: none;
-		z-index: 999;
-	}
-
-	& > svg {
-		position: absolute;
-		cursor: pointer;
-		top: 2rem;
-		right: 2rem;
-		color: red;
-		// 파람으로 보낼 데이터의 디폴트와 변수를 구분해서 삼항 연산자로 정리
-	}
 `
 
 const DescContainer = styled.div`
