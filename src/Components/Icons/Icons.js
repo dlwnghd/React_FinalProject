508ac3861b013927f0c008795ee3f713e7ad35e2
beenbin <77373566+showme0241@users.noreply.github.com>
2023-04-30 16:25:32 +0900

    :lipstick: Feat : 헤더에 사용되는 아이콘 파람받아서 react icons 적용

--- src/Components/Icons/Icons.js
+++ src/Components/Icons/Icons.js
@@ -21,7 +21,9 @@ import {
 } from 'react-icons/ai'
 
 export const Search_Icon = () => <AiOutlineSearch /> // 검색
-export const Arrow_Icon = () => <AiOutlineArrowRight /> // 슬라이드 화살표
+export const Arrow_Icon = ({ size }) => (
+	<AiOutlineArrowRight size={size ? size : 50} />
+) // 슬라이드 화살표
 
 export const NotFillHeart_Icon = ({ size, onClick }) => (
 	<AiOutlineHeart size={size ? size : 50} onClick={onClick} />
@@ -29,17 +31,23 @@ export const NotFillHeart_Icon = ({ size, onClick }) => (
 export const FillHeart_Icon = ({ size, onClick }) => (
 	<AiFillHeart size={size ? size : 50} onClick={onClick} />
 ) // 찬 하트
-export const Profile_Icon = () => <AiOutlineUser /> // 프로필
+export const Profile_Icon = ({ size }) => (
+	<AiOutlineUser size={size ? size : 50} />
+) // 프로필
 export const DropdownArrow_Icon = () => <AiFillCaretDown /> // 드롭 다운 화살표
 export const PaginationArrowSingle_Icon = () => <AiFillCaretRight /> // 페이지네이션화살표 (>)
 export const PaginationArrowDouble_Icon = () => <AiFillForward /> // 페이지네이션화살표 (>>)
 export const AddProduct_Icon = () => <AiOutlinePlusCircle /> // 상품 등록
-export const MenuBurger_Icon = () => <AiOutlineMenu /> // 햄버거
-export const InterestBasket_Icon = () => <AiOutlineShoppingCart /> // 관심 상품
+export const MenuBurger_Icon = ({ size, color, cursor }) => (
+	<AiOutlineMenu size={size} color={color} cursor={cursor} />
+) // 햄버거
+export const InterestBasket_Icon = ({ size, color, cursor }) => (
+	<AiOutlineShoppingCart size={size} color={color} cursor={cursor} />
+) // 관심 상품
 export const Camera_Icon = () => <AiOutlineCamera /> // 이미지 추가
 export const ChatOut_Icon = () => <AiOutlineExport /> // 채팅방 나가기
-export const EtcOption_Icon = ({ size }) => (
-	<AiOutlineMore size={size ? size : 50} />
+export const EtcOption_Icon = ({ size, onClick }) => (
+	<AiOutlineMore size={size ? size : 50} onClick={onClick} />
 ) // 기타 옵션
 
 // 탭바
