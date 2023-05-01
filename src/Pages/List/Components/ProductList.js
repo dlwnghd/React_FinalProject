4995296d542f3aa5cb558d25b24f28dd0bdc6a2e
dlwnghd <alion0513@naver.com>
2023-05-02 00:12:28 +0900

    🎨 Refactor : 옵저버 코드 수정

--- src/Pages/List/Components/ProductList.js
+++ src/Pages/List/Components/ProductList.js
@@ -1,18 +1,19 @@
 import axios from 'axios'
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import ItemBox from '../../../Components/ItemBox/ItemBox'
 import { ColumnNumberCSS, GridCenterCSS } from '../../../Styles/common'
 
 function ProductListWrapper() {
+	const navigate = useNavigate()
 	const [productList, setProductList] = useState(() => []) //Product List
 	const [page, setPage] = useState(1) //현재 페이지
-	const preventRef = useRef(true) //옵저버 중복 실행 방지
+	const preventRef = useRef(false) //옵저버 중복 실행 방지
 	const obsRef = useRef(null) //observer Element
 
 	useEffect(() => {
 		//옵저버 생성
-		console.log('옵저버 인식')
 		const observer = new IntersectionObserver(obsHandler, { threshold: 0.5 })
 		if (obsRef.current) observer.observe(obsRef.current)
 		return () => {
@@ -24,14 +25,17 @@ function ProductListWrapper() {
 		getProduct()
 	}, [page])
 
+	// 옵저버 핸들러
 	const obsHandler = entries => {
-		console.log('핸들러 실행')
-		setPage(prev => prev + 1)
+		const target = entries[0]
+		if (target.isIntersecting && preventRef.current) {
+			preventRef.current = false
+			setPage(prev => prev + 1)
+		}
 	}
 
+	//상품 불러오기
 	const getProduct = useCallback(async () => {
-		//상품 불러오기
-		console.log('상품 불러오기!')
 		try {
 			const res = await axios.get('/api/products', {
 				params: {
@@ -40,6 +44,7 @@ function ProductListWrapper() {
 				},
 			})
 			setProductList(prev => [...prev, ...res.data]) //리스트 추가
+			preventRef.current = true
 		} catch (e) {
 			console.error(e)
 		} finally {
