b512bf504e298b77ba6275a892e91c8099ca75a5
dlwnghd <alion0513@naver.com>
2023-05-02 17:23:22 +0900

    :lipstick: Design : Header, Sidebar 수정
    [mobile]
    - mobile 좌측 버튼 ➡️ 뒤로가기 버튼으로 수정
    - mobile 우측 버튼 클릭시 사이드 바 활성화 되도록 수정
    - sidebar 활성화시 아이콘 색상 변경되도록 수정
    
    [header]
    - 로그인 / 회원가입 페이지로 이동되도록 수정
    - 사용자 버튼 클릭시 활성화 되는 dropdown css 수정

--- src/Components/Layout/Header/Components/Sidebar.js
+++ src/Components/Layout/Header/Components/Sidebar.js
@@ -3,67 +3,29 @@ import { useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router'
 import { FlexAlignCSS } from '../../../../Styles/common'
 
-function Sidebar({
-	hamburgerShow,
-	setHamburgerShow,
-	selectedNav,
-	setSelectedNav,
-}) {
+function Sidebar({ interestedProductShow, setInterestedProductShow }) {
 	const navigate = useNavigate()
 	const slideRef = useRef()
 
 	useEffect(() => {
-		if (hamburgerShow === false) {
-			slideRef.current.style.transform = 'translateX(-100%)'
+		if (interestedProductShow === false) {
+			slideRef.current.style.transform = 'translateX(100%)'
 		}
-		if (hamburgerShow === true) {
+		if (interestedProductShow === true) {
 			slideRef.current.style.transform = 'translateX(0%)'
 		}
 	})
 
 	return (
-		<S.HamburgerMenu>
-			<S.SideBarContainer ref={slideRef}>
-				<ul>
-					<li
-						className={selectedNav === 0 ? 'selected' : ''}
-						onClick={() => {
-							navigate('/list/무료나눔리스트')
-							setHamburgerShow(false)
-							setSelectedNav(0)
-						}}
-					>
-						FREE MARKET
-					</li>
-					<li
-						className={selectedNav === 1 ? 'selected' : ''}
-						onClick={() => {
-							navigate('/list/중고거래리스트')
-							setHamburgerShow(false)
-							setSelectedNav(1)
-						}}
-					>
-						TRADE USED
-					</li>
-					<li
-						className={selectedNav === 2 ? 'selected' : ''}
-						onClick={() => {
-							navigate('/recent-price')
-							setHamburgerShow(false)
-							setSelectedNav(2)
-						}}
-					>
-						MARKET TREND
-					</li>
-				</ul>
-			</S.SideBarContainer>
-		</S.HamburgerMenu>
+		<S.InterestedProductMenu>
+			<S.SideBarContainer ref={slideRef}></S.SideBarContainer>
+		</S.InterestedProductMenu>
 	)
 }
 
 export default Sidebar
 
-const HamburgerMenu = styled.div`
+const InterestedProductMenu = styled.div`
 	${FlexAlignCSS}
 
 	@media screen and (min-width: 441px) {
@@ -74,7 +36,7 @@ const HamburgerMenu = styled.div`
 const SideBarContainer = styled.nav`
 	position: fixed;
 	transition: 0.5s ease-in-out;
-	transform: translateX(-100%);
+	transform: translateX(100%);
 	font-size: ${({ theme }) => theme.FONT_SIZE.medium};
 	font-family: ${({ theme }) => theme.FONT_WEIGHT.regular};
 	background-color: ${({ theme }) => theme.COLOR.common.gray[100]};
@@ -84,24 +46,9 @@ const SideBarContainer = styled.nav`
 	height: 100%;
 	top: 0;
 	left: 0;
-
-	& > ul {
-		padding: 12rem 4rem;
-	}
-
-	& > ul > li {
-		margin-bottom: 4rem;
-		cursor: pointer;
-
-		/* 선택된 항목에만 box_shadow 추가 */
-		&.selected {
-			font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
-			box-shadow: rgb(25, 31, 40) 0px -3px 0px inset;
-		}
-	}
 `
 
 const S = {
-	HamburgerMenu,
+	InterestedProductMenu,
 	SideBarContainer,
 }
--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -9,9 +9,9 @@ import { useEffect, useRef, useState } from 'react'
 import Sidebar from './Components/Sidebar'
 import {
 	InterestBasket_Icon,
-	MenuBurger_Icon,
 	Profile_Icon,
 	Search_Icon,
+	RollBack_icon,
 } from '../../Icons/Icons'
 
 function Header({ searchProduct }) {
@@ -20,12 +20,12 @@ function Header({ searchProduct }) {
 	const [product, setProduct] = useState('') // 검색할 물품 State관리용
 	const [selectedNav, setSelectedNav] = useState(-1) // 선택된 Navigation 항목의 인덱스
 
-	const [login, setLogin] = useState(true) // 로그인 Header 구현용 State
+	const [login, setLogin] = useState(false) // 로그인 Header 구현용 State
 
 	const userMenu = useRef() // 사용자 드롭다운 이외의 영역 클릭시 닫는용 Ref
 	const [dropdown, setDropdown] = useState(true) // 사용자 드롭다운 관리용
 
-	const [hamburgerShow, setHamburgerShow] = useState(false) // 모바일 햄버거메뉴 활성화용
+	const [interestedProductShow, setInterestedProductShow] = useState(false) // 모바일 햄버거메뉴 활성화용
 
 	/**
 	 * 드롭다운 닫기 핸들러
@@ -83,7 +83,8 @@ function Header({ searchProduct }) {
 							}}
 						>
 							<Profile_Icon size="28" />
-							<p>로그인 / 회원가입</p>
+							<span>99+</span>
+							<p>회원명</p>
 						</S.UserBox>
 						{dropdown && (
 							<UserDropDownMenu>
@@ -137,12 +138,12 @@ function Header({ searchProduct }) {
 				)}
 				<S.List>
 					<div>
-						<S.MobileIcon
-							onClick={() => {
-								setHamburgerShow(prev => !prev)
-							}}
-						>
-							<MenuBurger_Icon size="24" color="white" cursor="pointer" />
+						<S.MobileIcon onClick={() => navigate(-1)}>
+							<RollBack_icon
+								size="24"
+								color={interestedProductShow ? 'black' : 'white'}
+								cursor="pointer"
+							/>
 						</S.MobileIcon>
 						<S.Logo
 							onClick={() => {
@@ -152,8 +153,16 @@ function Header({ searchProduct }) {
 						>
 							NEGO MARKET
 						</S.Logo>
-						<S.MobileIcon>
-							<InterestBasket_Icon size="24" color="white" cursor="pointer" />
+						<S.MobileIcon
+							onClick={() => {
+								setInterestedProductShow(prev => !prev)
+							}}
+						>
+							<InterestBasket_Icon
+								size="24"
+								color={interestedProductShow ? 'black' : 'white'}
+								cursor="pointer"
+							/>
 						</S.MobileIcon>
 					</div>
 					<S.SearchContainer>
@@ -169,10 +178,8 @@ function Header({ searchProduct }) {
 					</S.SearchContainer>
 				</S.List>
 				<Sidebar
-					hamburgerShow={hamburgerShow}
-					setHamburgerShow={setHamburgerShow}
-					selectedNav={selectedNav}
-					setSelectedNav={setSelectedNav}
+					interestedProductShow={interestedProductShow}
+					setInterestedProductShow={setInterestedProductShow}
 				/>
 				<S.Bottom>
 					<S.NavItem
@@ -282,6 +289,8 @@ const List = styled.div`
  * 로그인 / 회원가입
  */
 const Login_Join = styled.div`
+	height: 4rem;
+	color: white;
 	text-align: right;
 
 	& > span {
@@ -378,36 +387,6 @@ const NavItem = styled.div`
 	}
 `
 
-// /**
-//  * 모바일 검색창
-//  */
-// const MobileSearchContainer = styled.div`
-// 	display: none;
-// 	width: 100%;
-// 	position: relative;
-
-// 	@media screen and (max-width: 440px) {
-// 		display: block;
-// 	}
-
-// 	& > input {
-// 		width: 100%;
-// 		height: 4rem;
-// 		box-sizing: border-box;
-// 		border-radius: 2rem;
-// 		text-indent: 4.4rem;
-// 		border: 0.2rem solid #aaa;
-// 		font-size: ${({ theme }) => theme.FONT_SIZE.tiny};
-// 	}
-
-// 	& > svg {
-// 		position: absolute;
-// 		left: 1rem;
-// 		font-size: 2.8rem;
-// 		color: black;
-// 	}
-// `
-
 /**
  * 모바일 아이콘 구역
  */
@@ -418,6 +397,7 @@ const MobileIcon = styled.div`
 		display: block;
 
 		& > svg {
+			transition: color 1s;
 			position: relative;
 			z-index: 9999;
 		}
@@ -445,6 +425,19 @@ const UserBox = styled.div`
 	& > svg {
 		margin-right: 1rem;
 	}
+
+	& > span {
+		position: absolute;
+		transform: translate(2.2rem, -1rem);
+		background: red;
+		border: 1px solid white;
+		border-radius: 50rem;
+		padding: 0px 5px;
+	}
+
+	& > p {
+		margin-left: 2.5rem;
+	}
 `
 const UserDropDownMenu = styled.div`
 	position: absolute;
@@ -452,14 +445,19 @@ const UserDropDownMenu = styled.div`
 	background-color: #acacac;
 	border: 1px solid black;
 	border-radius: 5%;
+	top: 25%;
+	z-index: 9999;
 
 	& > span {
 		padding: 1rem;
+		border: 1px solid white;
 		cursor: pointer;
+		border-radius: 5%;
 
 		:hover {
 			font-family: ${({ theme }) => theme.FONT_WEIGHT.bold};
 			background-color: ${({ theme }) => theme.COLOR.hover};
+			border: 1px solid white;
 		}
 	}
 `
