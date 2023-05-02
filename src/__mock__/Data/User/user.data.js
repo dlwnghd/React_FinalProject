18a3c3c3a341dcf1404f54d6f44c0672b914fc2c
Doeunnkimm <doeunnkimm@gmail.com>
2023-05-02 12:49:23 +0900

    :sparkles: Feat : 물품 등록, 회원정보 조회, 회원정보 수정 mock api 추가

--- src/__mock__/Data/User/user.data.js
+++ src/__mock__/Data/User/user.data.js
@@ -4,8 +4,10 @@ const userMock = [
 		email: 'test@test.com',
 		pw: 'test1111!',
 		nickName: '테스트닉네임',
-		phone: '01011112222',
+		phone: '010-1111-2222',
 		region: '서울 강남구 역삼동',
+		profile_img: null,
+		token: 11111,
 	},
 ]
 
