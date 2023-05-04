c0e2c84bb0aebf313ba54a86a58ea3be7930342e
Doeunnkimm <doeunnkimm@gmail.com>
2023-05-04 14:04:50 +0900

    :sparkles: Feat : axios.create, UserApi 모듈화
    UserApi에는 로그인, 회원가입, 이메일 중복검사, 닉네임 중복검사, 로그아웃, 리프레시 토큰 재발급 요청 로직이 포함되어 있습니다.

---
+++ src/Apis/@core.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import TokenService from '../Utils/tokenService'
+import AuthApi from './authApi'
+import LOCAL_STORAGE_KEY from '../Consts/storage.key'
+
+const axiosInstance = axios.create({
+	baseURL: process.env.REACT_APP_BACKEND_URL,
+	withCredentials: true,
+})
+
+axiosInstance.interceptors.request.use(
+	config => {
+		const access_token = TokenService.getAccessToken()
+		if (access_token) {
+			config.headers.Authorization = `Bearer ${access_token}`
+			return config
+		}
+	},
+	error => {
+		return Promise.reject(error)
+	},
+)
+
+axiosInstance.interceptors.response.use(
+	res => {
+		return res
+	},
+	async error => {
+		if (error.response.status === 417) {
+			// access token 재발급 필요
+			AuthApi.logout()
+			TokenService.removeAccessToken(LOCAL_STORAGE_KEY.ACCESS_TOKEN) // 기존 access token 삭제
+		}
+		const originalRequest = error.config
+		if (error.response.status === 403 && !originalRequest._retry) {
+			// refresh 관련 세션 만료
+			originalRequest._retry = true // 재요청 보냄을 의미
+			const res = await AuthApi.refreshToken()
+			if (res.status === 200) {
+				// access_token 다시 세팅
+				const token = res.data.token
+				TokenService.setAccessToken(token) // 새로운 access token 세팅
+				axiosInstance.defaults.headers.common[
+					'Authorization'
+				] = `Bearer ${token}`
+
+				return axiosInstance(originalRequest) // 재요청
+			}
+		}
+		return Promise.reject(error)
+	},
+)
+
+export default axiosInstance
