d67fadca66465bf4bac9a60ecbacaef3ee83ad99
Doeunnkimm <doeunnkimm@gmail.com>
2023-05-06 15:33:28 +0900

    :sparkles: Feat : 웹 스토리지와 atom을 한번에 관리하는 hook함수 생성

--- src/Utils/userInfoService.js
+++ src/Utils/userInfoService.js
@@ -10,6 +10,12 @@ const UserInfoService = {
 	removeUserInfo() {
 		localStorage.removeItem(LOCAL_STORAGE_KEY.USER_INFO)
 	},
+	setSaveId(email) {
+		localStorage.setItem(LOCAL_STORAGE_KEY.SAVE_ID, email)
+	},
+	getSavedId() {
+		return localStorage.getItem(LOCAL_STORAGE_KEY.SAVE_ID)
+	},
 }
 
 export default UserInfoService
