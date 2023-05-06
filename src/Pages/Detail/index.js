26fcd94dfea33f320761854df77f6429f3fc4fa4
beenbin <77373566+showme0241@users.noreply.github.com>
2023-05-06 16:03:41 +0900

    :lipstick: Design : Detail 페이지 코드 구조 개선 작업

--- src/Pages/Detail/index.js
+++ src/Pages/Detail/index.js
@@ -1,17 +1,17 @@
-
 import styled from 'styled-components'
-import ProductInfo from './Components/ProductInfo'
 import {
 	ColumnNumberCSS,
 	GridCenterCSS,
 	WidthAutoCSS,
 } from '../../Styles/common'
 import productsMock from '../../__mock__/Data/Product/product.data'
-import { useState ,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import RecentBanner from '../Main/Components/Banner/RecentBanner'
 import { useParams } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
 import { isOnSideBar } from '../../Atoms/sideBar.atom'
+import Thumbnail from './Components/Thumbnail/Thumbnail'
+import Description from './Components/Description/Description'
 
 function Detail() {
 	//서버로부터 상품들을 요청하고 idx값과 일치하는 것 찾기
@@ -20,21 +20,19 @@ function Detail() {
 	// ex ) const findproduct = productsMock.find(item => item.idx === idx)
 
 	const [product, setProduct] = useState(productsMock[0])
-
-
-
-function Detail() {
-	const params = useParams()
+	const { ProductImages } = product
 	const [onSideBar, setOnSideBar] = useRecoilState(isOnSideBar)
 
 	useEffect(() => {
 		setOnSideBar(false)
-	}, [params])
-
+	}, [idx])
 
 	return (
 		<S.Wrapper>
-			<ProductInfo product={product} />
+			<S.MainContainer>
+				<Thumbnail productImages={ProductImages} />
+				<Description product={product} />
+			</S.MainContainer>
 			<Maps>
 				<div>지도</div>
 			</Maps>
@@ -54,28 +52,42 @@ function Detail() {
 
 export default Detail
 
-const Wrapper = styled.div`
-	padding: 12rem 0;
+const Wrapper = styled.section`
 	${WidthAutoCSS}
+	margin: 12rem auto;
 	${GridCenterCSS}
 	${ColumnNumberCSS(1)}
+
+	@media screen and (max-width:${({ theme }) => theme.MEDIA.tablet}) {
+		margin: 3rem auto;
+	}
+`
+
+const MainContainer = styled.section`
+	${GridCenterCSS}
+	${ColumnNumberCSS(2)}
+
+	@media screen and (max-width:${({ theme }) => theme.MEDIA.tablet}) {
+		${ColumnNumberCSS(1)}
+	}
 `
 const Maps = styled.div`
 	width: 100%;
-	border: 1px solid black;
+	/* border: 1px solid black; */
+	background: ${({ theme }) => theme.COLOR.common.gray[100]};
 	height: 25rem;
-	@media screen and (max-width: ${({ theme }) => theme.MEDIA.mobile};) {
+	@media screen and (max-width: ${({ theme }) => theme.MEDIA.mobile}) {
 		width: 100%;
 	}
 `
 const PrdListBox = styled.div`
 	width: 100%;
-	border: 1px soild black;
+	/* border: 1px solid black; */
 
 	${GridCenterCSS}
 	${ColumnNumberCSS(1)}
 
-	@media screen and (max-width: ${({ theme }) => theme.MEDIA.mobile};) {
+	@media screen and (max-width: ${({ theme }) => theme.MEDIA.mobile}) {
 		width: 95%;
 	}
 `
@@ -101,10 +113,11 @@ const AnotherPrdList = styled.div`
 	& > div {
 		width: 100%;
 		height: 20rem;
-		border: 1px solid black;
+		background: ${({ theme }) => theme.COLOR.common.gray[100]};
 	}
 `
 const S = {
 	Wrapper,
+	MainContainer,
 	PrdListBox,
 }
