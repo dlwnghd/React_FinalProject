03f37c02be0c8aff7226812f3d1a33a2b3366c63
jacobYun97 <ydy970527@gmail.com>
2023-05-06 14:38:02 +0900

    :sparkles: Feat : Filter 함수 기능

--- src/Pages/RecentPrice/index.js
+++ src/Pages/RecentPrice/index.js
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+import Filter from '../../Components/Filter/Filter'
+
+import Graph from './Components/Graph/Graph'
+import RecentSearch from './Components/Search/Search'
+
 function RecentPrice() {
+	const dateFilter = ['3개월', '6개월', '1년']
+	const [filterOption, setFilterOption] = useState(dateFilter[0])
+	const onFilter = e => {
+		switch (e.target.innerText) {
+			case dateFilter[0]:
+				setFilterOption(dateFilter[0])
+				break
+			case dateFilter[1]:
+				setFilterOption(dateFilter[1])
+				break
+			case dateFilter[2]:
+				setFilterOption(dateFilter[2])
+				break
+			default:
+				break
+		}
+	}
 	return (
 		<>
-			<div></div>
+			<RecentSearch />
+			<Filter filterArray={dateFilter} onClick={onFilter} />
+			<Graph filterOption={filterOption} />
 		</>
 	)
 }
