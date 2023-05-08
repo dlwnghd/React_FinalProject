29595439c4405b685d9a79e16f19390fad4ac0f2
Doeunnkimm <doeunnkimm@gmail.com>
2023-05-08 23:43:47 +0900

    :fire: Remove : 테스트 중인 코드 우선 삭제

--- src/Routes/private.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-import TokenService from '../Utils/tokenService'
-
-function PrivateRoute() {
-	const access_token = TokenService.getAccessToken()
-
-	return access_token ? <Outlet /> : <Navigate to={'/login'} />
-}
-export default PrivateRoute
