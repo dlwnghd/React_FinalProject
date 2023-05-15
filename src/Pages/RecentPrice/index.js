d3a7f5589a3f6f283d53a3a6d7aa086b7fe70166
dlwnghd <alion0513@naver.com>
2023-05-15 16:13:05 +0900

    ✨ Feat : 더미데이터 이용한 최근 거래 종료 품목 UI 추가

--- src/Pages/RecentPrice/index.js
+++ src/Pages/RecentPrice/index.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import styled from 'styled-components'
 import { WidthAutoCSS } from '../../Styles/common'
+import productsMock from '../../__mock__/Data/Product/product.data'
 import recentPriceMock from '../../__mock__/Data/Product/recentPrice.data'
 import Graph from './Components/Graph'
 import RecentSearch from './Components/Search'
+import SoldOutList from './Components/SoldOutList'
 
 function RecentPrice() {
 	const [dummyData, setDummyData] = useState(recentPriceMock)
@@ -25,6 +27,7 @@ function RecentPrice() {
 			{searchPrd && (
 				<Graph dummyData={searchPrd} avgPrice={avgPrice} search={search} />
 			)}
+			<SoldOutList soldOutList={productsMock.slice(0, 10)} />
 		</S.RecentPriceWrapper>
 	)
 }
