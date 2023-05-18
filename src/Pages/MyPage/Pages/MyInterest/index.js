8e1395664b552897ee65c8ff866cfda946ea9c94
twosky0202 <leehaneul0202@naver.com>
2023-05-18 09:09:31 +0900

    :lipstick: Design : 관심 상품 조회 UI 구성

--- src/Pages/MyPage/Pages/MyInterest/index.js
+++ src/Pages/MyPage/Pages/MyInterest/index.js
@@ -1,9 +1,53 @@
+import styled from 'styled-components'
+import productsMock from '../../../../__mock__/Data/Product/product.data'
+import {
+	ColumnNumberCSS,
+	GridCenterCSS,
+	WidthAutoCSS,
+} from '../../../../Styles/common'
+import ItemBox from '../../../../Components/ItemBox/ItemBox'
+import { useState } from 'react'
+
 function MyInterest() {
+	const [myInterest, setMyInterest] = useState()
+
 	return (
-		<>
-			<div></div>
-		</>
+		<S.Wrapper>
+			<S.Container>
+				{productsMock.map((item, index) => {
+					// myInterest.map((item, index) => {
+					return (
+						<ItemBox
+							title={item.title}
+							price={item.price}
+							posterPath={item.image_url}
+							context={item.description}
+							isLiked={item.liked}
+							key={index}
+							onClick={() => navigate(`/detail/${item.idx}`)}
+						/>
+					)
+				})}
+			</S.Container>
+		</S.Wrapper>
 	)
 }
 
 export default MyInterest
+
+const Wrapper = styled.div`
+	${WidthAutoCSS}
+`
+const Container = styled.div`
+	${GridCenterCSS}
+	${ColumnNumberCSS(4)};
+	@media screen and (max-width: ${({ theme }) => theme.MEDIA.mobile}) {
+		${ColumnNumberCSS(2)}
+		gap: 3rem 1rem;
+	}
+`
+
+const S = {
+	Wrapper,
+	Container,
+}
