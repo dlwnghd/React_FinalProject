423ec8a1672e3d2602f2e68f77f34cfd938cf818
Doeunnkimm <doeunnkimm@gmail.com>
2023-05-25 02:36:02 +0900

    :sparkles: Feat : 마이페이지 메인 데이터 로딩 시 Skeleton UI 추가

--- src/Pages/MyPage/Components/Header/index.js
+++ src/Pages/MyPage/Components/Header/index.js
@@ -2,23 +2,25 @@ import Profile from './Components/Profile'
 import useGetMyPageMainData from '../../../../Hooks/Queries/get-myPageMain'
 import MyPageNav from './Components/Navigation'
 import ErrorFallback from '../../../../Components/Error/ErrorFallback'
+import ProfileSkeleton from './Components/ProfileSkeleton'
 
 function MyPageIndex() {
 	const { data: mainData, status, error } = useGetMyPageMainData()
 
-	if (status === 'loading') return // 로딩 중일 때
+	if (status === 'loading') {
+		return <ProfileSkeleton />
+	}
 
-	if (error) {
+	if (status === 'error') {
 		return <ErrorFallback error={error} />
 	}
 
-	if (!error)
-		return (
-			<>
-				<Profile mainData={mainData} />
-				<MyPageNav type={'myPage'} />
-			</>
-		)
+	return (
+		<>
+			<Profile mainData={mainData} />
+			<MyPageNav type={'myPage'} />
+		</>
+	)
 }
 
 export default MyPageIndex
