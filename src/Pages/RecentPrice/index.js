ad5e7b09501f0cc2ffe3602b7bfe3c5262e0ac0d
Doeunnkimm <doeunnkimm@gmail.com>
2023-06-03 15:03:49 +0900

    :sparkles: Feat : 시세 동향 - 최근 거래 종료 품목 로딩 시 스켈레톤 처리

--- src/Pages/RecentPrice/index.js
+++ src/Pages/RecentPrice/index.js
@@ -3,12 +3,18 @@ import { useState } from 'react'
 import styled from 'styled-components'
 import Filter from './Components/DateFilter'
 import useGetQuoteProductList from '../../Hooks/Queries/get-quoteList'
-import { FlexBetweenCSS, WidthAutoCSS } from '../../Styles/common'
+import {
+	ColumnNumberCSS,
+	FlexBetweenCSS,
+	GridCenterCSS,
+	WidthAutoCSS,
+} from '../../Styles/common'
 import Graph from './Components/Graph'
 import RecentSearch from './Components/Search'
 import SoldOutList from './Components/SoldOutList'
 import getFormattedDate from '../../Utils/getFormattedDate'
 import { useSearchParams } from 'react-router-dom'
+import MainSkeleton from '../../Components/ItemBox/ItemSkeleton'
 
 function RecentPrice() {
 	// Filter 종류
@@ -85,7 +91,7 @@ function RecentPrice() {
 						searchQuote={searchQuote}
 					/>
 				</S.OptionContainer>
-				{data ? (
+				{!isLoading ? (
 					<Graph quoteList={data} searchQuote={searchQuote} />
 				) : (
 					<S.SkeletonBox>
@@ -99,7 +105,18 @@ function RecentPrice() {
 					</S.SkeletonBox>
 				)}
 			</S.RecentPriceContainer>
-			<SoldOutList soldOutList={data} />
+			<S.SoldOutListWrapper>
+				<h3>최근 거래 종료 품목</h3>
+				<S.SoldOutListContainer>
+					{!isLoading ? (
+						<SoldOutList soldOutList={data} />
+					) : (
+						Array(8)
+							.fill('')
+							.map((_, i) => <MainSkeleton />)
+					)}
+				</S.SoldOutListContainer>
+			</S.SoldOutListWrapper>
 		</S.RecentPriceWrapper>
 	)
 }
@@ -137,9 +154,26 @@ const SkeletonBox = styled.div`
 	}
 `
 
+const SoldOutListWrapper = styled.div``
+
+const SoldOutListContainer = styled.div`
+	width: 100%;
+	margin-top: 4rem;
+	margin-bottom: 8rem;
+	${GridCenterCSS}
+	${ColumnNumberCSS(4)};
+
+	@media screen and (max-width: ${({ theme }) => theme.MEDIA.mobile}) {
+		${ColumnNumberCSS(2)}
+		column-gap: 1rem;
+	}
+`
+
 const S = {
 	RecentPriceWrapper,
 	RecentPriceContainer,
 	OptionContainer,
 	SkeletonBox,
+	SoldOutListWrapper,
+	SoldOutListContainer,
 }
