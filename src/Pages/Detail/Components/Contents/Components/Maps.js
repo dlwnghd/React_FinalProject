fa3b7c6004c8f81e546673ab711e5b2162016a5e
taeginote <ab315601@naver.com>
2023-06-06 18:20:09 +0900

    💄Design : 모바일 시세동향 크기 수정

--- src/Pages/Detail/Components/Contents/Components/Maps.js
+++ src/Pages/Detail/Components/Contents/Components/Maps.js
@@ -1,38 +1,34 @@
 import { useEffect } from 'react'
 import styled from 'styled-components'
 
-const { kakao } = window
+// const { kakao } = window
 
 function Maps({ detailProduct }) {
 	useEffect(() => {
-		const container = document.getElementById('myMap')
-		const options = {
-			center: new kakao.maps.LatLng(0, 0),
-			level: 3,
-		}
-
-		const map = new kakao.maps.Map(container, options)
-
-		// Geocoder : 주소나 장소명을 좌표로 변환 가능
-		const geocoder = new kakao.maps.services.Geocoder()
-		geocoder.addressSearch(
-			detailProduct.searchProduct.region,
-			(result, status) => {
-				if (status === kakao.maps.services.Status.OK) {
-					// 검색어 기반 좌표 설정
-					const { y: latitude, x: longitude } = result[0]
-					const markerPosition = new kakao.maps.LatLng(latitude, longitude)
-
-					// 옵션과 좌표를 기반하여 마커 생성
-					const marker = new kakao.maps.Marker({
-						position: markerPosition,
-					})
-
-					marker.setMap(map)
-					map.setCenter(markerPosition)
-				}
-			},
-		)
+		// 	const container = document.getElementById('myMap')
+		// 	const options = {
+		// 		center: new kakao.maps.LatLng(0, 0),
+		// 		level: 3,
+		// 	}
+		// 	const map = new kakao.maps.Map(container, options)
+		// 	// Geocoder : 주소나 장소명을 좌표로 변환 가능
+		// 	const geocoder = new kakao.maps.services.Geocoder()
+		// 	geocoder.addressSearch(
+		// 		detailProduct.searchProduct.region,
+		// 		(result, status) => {
+		// 			if (status === kakao.maps.services.Status.OK) {
+		// 				// 검색어 기반 좌표 설정
+		// 				const { y: latitude, x: longitude } = result[0]
+		// 				const markerPosition = new kakao.maps.LatLng(latitude, longitude)
+		// 				// 옵션과 좌표를 기반하여 마커 생성
+		// 				const marker = new kakao.maps.Marker({
+		// 					position: markerPosition,
+		// 				})
+		// 				marker.setMap(map)
+		// 				map.setCenter(markerPosition)
+		// 			}
+		// 		},
+		// 	)
 	}, [])
 
 	return (
