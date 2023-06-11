5dd1508d672a96827630b2361bda5102fdaaca20
beenbin <77373566+showme0241@users.noreply.github.com>
2023-06-11 15:25:31 +0900

    :ambulance: Fix : Detail > Maps 컴포넌트 import

--- src/Pages/Detail/Components/Contents/Contents.js
+++ src/Pages/Detail/Components/Contents/Contents.js
@@ -5,6 +5,7 @@ import useGetMyPageInterestData from '../../../../Hooks/Queries/get-myPageIntere
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import useGetMainPageData from '../../../../Hooks/Queries/get-mainPage'
+import Maps from './Components/Maps'
 
 function Contents({ detailProduct }) {
 	const [page, setPage] = useState(1)
